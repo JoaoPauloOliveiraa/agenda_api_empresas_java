65e0b544159241d4947916704a097c8aa32ea289
João Paulo <joaopaulo.oliveira83@gmail.com>
2021-12-07 11:38:11 -0300

    EmpresasController finalizada com os metodos get, post, put e delete

--- src/main/java/com/desafio21diasmultiplasapis/empresas/controllers/EmpresasController.java
+++ src/main/java/com/desafio21diasmultiplasapis/empresas/controllers/EmpresasController.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 import com.desafio21diasmultiplasapis.empresas.models.Empresa;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +19,12 @@ public class EmpresasController {
     @GetMapping("/empresas")
     public List<Empresa> index(){
         List<Empresa> empresas = new ArrayList<Empresa>();
+        empresas.add(new Empresa());
+        empresas.add(new Empresa());
+        empresas.add(new Empresa());
+        empresas.add(new Empresa());
+        empresas.add(new Empresa());
+        empresas.add(new Empresa());
         return empresas;
     }
 
@@ -24,6 +34,24 @@ public class EmpresasController {
         Empresa empresa = new Empresa();
         empresa.setId(id);
         return empresa;
+    }
+
+    @PostMapping("/empresas")
+    public Empresa create(@RequestBody Empresa empresa)
+    {
+        return empresa;
+    }
+
+    @PutMapping("/empresas/{id}")
+    public Empresa update(@PathVariable int id, @RequestBody Empresa empresa)
+    {
+        return empresa;
+    }
+
+    @DeleteMapping("/empresas/{id}")
+    public Empresa delete(@PathVariable int id)
+    {
+        return new Empresa();
     }
 
 }
